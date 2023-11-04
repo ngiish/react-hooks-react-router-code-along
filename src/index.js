53909c9e15e5749bbff8fbcd1b88d06fa360fb0c
Isaac <govegeku00@gmail.com>
2023-11-04 19:06:39 +0300

    New Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //Step 1. Import react-router functions
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 function Home() {
   return (
@@ -30,21 +30,28 @@ function Login() {
         <div>
           <input type="password" name="password" placeholder="Password"/>
         </div>
-        <input type="submit" value={Submit}/>
+        <input type="submit" value="Submit"/>
       </form>
     </div>
   )
 }
 
 //Step 2. Change so router is coordinating what is displayed
-// function App() {
-//   return <Home />;
-// }
+//Let's add our about and login routes to our router
+// If we had a header component we wanted to be displayed no matter which route was hit, this behavior would be desirable. 
+// Otherwise, there are several ways to fix this. 
+// One way to give more predictable behavior to our Routes is to use the Switch component:
 
 ReactDOM.render(
   <BrowserRouter>
   <Route path="/">
     <Home />
+  </Route>
+  <Route path="/about">
+    <About />
+  </Route>
+  <Route path="/login">
+    <Login />
   </Route>
   </BrowserRouter>,
  document.getElementById("root"));
@@ -55,7 +62,5 @@ ReactDOM.render(
 //   The Route component has one prop in our example: path.
 // *The Route component is in charge of saying: "when the URL matches this specified path, render this child component." 
 // This handles the conditional rendering based on the URL that we described earlier.
-
-//ADDING ADDITIONAL ROUTES
 
 
