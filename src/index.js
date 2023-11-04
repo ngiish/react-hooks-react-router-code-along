f51a644d7b6b5934489fb7aa161ff6cf3f49f6f5
Isaac <govegeku00@gmail.com>
2023-11-04 20:04:35 +0300

    New Commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//Step 1. Import react-router functions
-import { BrowserRouter, Route} from "react-router-dom";
+/* Add NavLink to import */
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-function Home() {
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+/* define the NavBar component */
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
     </div>
   );
 }
 
+function Home() {
+  return <h1>Home!</h1>;
+}
+
 function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  )
+  return <h1>This is my about component!</h1>;
 }
 
 function Login() {
@@ -25,42 +69,32 @@ function Login() {
       <h1>Login</h1>
       <form>
         <div>
-          <input type="text" name="username" placeholder="Username"/>
+          <input type="text" name="username" placeholder="Username" />
         </div>
         <div>
-          <input type="password" name="password" placeholder="Password"/>
+          <input type="password" name="password" placeholder="Password" />
         </div>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </form>
     </div>
-  )
+  );
 }
 
-//Step 2. Change so router is coordinating what is displayed
-//Let's add our about and login routes to our router
-// If we had a header component we wanted to be displayed no matter which route was hit, this behavior would be desirable. 
-// Otherwise, there are several ways to fix this. 
-// One way to give more predictable behavior to our Routes is to use the Switch component:
-
+/* add the NavBar component to our render method */
 ReactDOM.render(
   <BrowserRouter>
-  <Route path="/">
-    <Home />
-  </Route>
-  <Route path="/about">
-    <About />
-  </Route>
-  <Route path="/login">
-    <Login />
-  </Route>
+    <NavBar /> 
+    
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    
   </BrowserRouter>,
- document.getElementById("root"));
-
- //I*n the code above there are two components that we're importing from React Router. We use them in turn:
-//  The BrowserRouter component is the base for our application's routing. 
-//  It is where we declare how React Router will be used. Notice that nested inside the BrowserRouter component we use the Route component.
-//   The Route component has one prop in our example: path.
-// *The Route component is in charge of saying: "when the URL matches this specified path, render this child component." 
-// This handles the conditional rendering based on the URL that we described earlier.
-
-
+  document.getElementById("root")
+);
